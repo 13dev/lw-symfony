11bf1265be90ad44c9bbcd1cdb347feab12a981d
Leo Oliveira <leo@13dev.pt>
2023-01-26 21:56:32 +0000

    feat(server): Create a ServerDto.php to pass data to the repo.

---
+++ src/Infra/Dto/ServerDto.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Infra\Dto;
+
+use App\Domain\Common\ValueObject\PriceValueObject;
+use App\Domain\Common\ValueObject\RamValueObject;
+use App\Domain\Common\ValueObject\StorageValueObject;
+use JetBrains\PhpStorm\Immutable;
+
+#[Immutable]
+class ServerDto
+{
+    public function __construct(
+        private readonly string $name,
+        private readonly RamValueObject $ram,
+        private readonly StorageValueObject $storage,
+        private readonly PriceValueObject $price,
+        private readonly string $location,
+    )
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getRam(): RamValueObject
+    {
+        return $this->ram;
+    }
+
+    public function getStorage(): StorageValueObject
+    {
+        return $this->storage;
+    }
+
+    public function getPrice(): PriceValueObject
+    {
+        return $this->price;
+    }
+
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+}
