e06cd20d4f543d61a156a5ad5a8ac8feaaed9dba
Leo Oliveira <leo@13dev.pt>
2023-01-26 22:05:23 +0000

    feat(common): Create a VO to RAM this class will parse the data of ram from the collection of data.

---
+++ src/Domain/Common/ValueObject/RamValueObject.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Common\ValueObject;
+
+use App\Domain\Common\DataUnitEnum;
+use App\Domain\Common\RamTypeEnum;
+use InvalidArgumentException;
+use JetBrains\PhpStorm\Immutable;
+use JsonSerializable;
+
+#[Immutable]
+class RamValueObject
+{
+    private const RAM_REGEX_EXPR = '/(\d+)(\w{2})(\w+)/';
+
+    public function __construct(
+        private readonly RamTypeEnum  $type,
+        private readonly DataUnitEnum $unit,
+        private readonly int          $capacity,
+    )
+    {
+    }
+
+    public static function fromString(string $value): self
+    {
+        $matches = [];
+        preg_match(self::RAM_REGEX_EXPR, $value, $matches);
+
+        [$capacity, $dataUnit, $type] = $matches;
+
+        if (empty($matches) || !isset($capacity, $dataUnit, $type)) {
+            throw new InvalidArgumentException('Invalid ram format.');
+        }
+
+        if (!$dataUnit = DataUnitEnum::tryFrom($dataUnit)) {
+            throw new InvalidArgumentException('Unknown data unit.');
+        }
+
+        if (!$type = RamTypeEnum::tryFrom($type)) {
+            throw new InvalidArgumentException('Unknown ram type.');
+        }
+
+        return new self(
+            type: $type,
+            unit: $dataUnit,
+            capacity: $capacity,
+        );
+
+    }
+
+    public function getUnit(): DataUnitEnum
+    {
+        return $this->unit;
+    }
+
+    public function getCapacity(): int
+    {
+        return $this->capacity;
+    }
+
+    public function getType(): RamTypeEnum
+    {
+        return $this->type;
+    }
+}
